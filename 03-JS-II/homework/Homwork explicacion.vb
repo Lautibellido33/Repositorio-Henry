ae614bcbe51d8d37492a0c5b8605d92a2ca4c560
Lautibellido33 <bellidolautaro33@gmail.com>
2021-09-22 20:30:27 -0300

    Tarea Henry

---
+++ 03-JS-II/homework/Homwork explicacion.vb
@@ -0,0 +1,12 @@
+for: tiene una sintaxis unica, pero compleja, se coloca la palabra calve for seguda de un parentesis () y despues abir 
+y cerrar corchetes {}
+Dentro de el parentesis necesitamos 3 cosas, 1: una variable, sobre la variable se repetira el bucle, 2: una exepresion
+condicional, 3: incrementar una variable, todas las declaraciones estanr separadas por punto y coma ;
+
+&&: Evalua AMBAS expresiones y devolvera true si las dos expresiones son verdaderas,
+    Si una (o ambas) expresiones son falsas devolvera false
+||: Determina si una de las expresiones es true. Devolvera true si una o ambas expresiones son verdaderas.
+    Devolvera false sin AMBAS expresiones son falsas
+ !: Este operador devuelve LO CONTRARIO al valor que se le pase ej:
+  if (! (1 === 1 ) ) false 
+      
